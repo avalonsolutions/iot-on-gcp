8cf29491b200234f757c767b389617fd0a3b1b8f
vg <vsevolod.hrechaniuk@avalonsolutions.com>
2019-08-09 17:11:30 +0200

    bash fix beam staging script

--- beam/deploy.sh
+++ beam/deploy.sh
@@ -1,14 +1,5 @@
 #!/bin/bash
 
-#python -m telemetry_etl \
-#    --runner DataflowRunner \
-#    --project seva-sandbox \
-#    --staging_location gs://seva-playground-dataflow-staging/bin \
-#    --temp_location gs://seva-playground-dataflow-staging/tmp \
-#    --template_location gs://seva-playground-dataflow-staging/templates/telemetry_etl
-
-#--region us-central1 \
-
 
 python -m telemetry_etl \
 --project seva-playground \
