469d658808881907d8fb195bdb244b0bce6308e2
vg <vsevolod.hrechaniuk@avalonsolutions.com>
2019-08-09 17:31:24 +0200

    bash fix args parsing

--- gce/init.sh
+++ gce/init.sh
@@ -1,13 +1,12 @@
 #!/bin/bash
 
 
-cd iot-on-gcp/gce
-export PROJECT_ID=$(gcloud config get-value project)
-export REGION="us-central1"
+echo $PROJECT_ID
+echo $REGION
 echo 'creating registry'
 gcloud beta iot registries create simulation \
    --project=$PROJECT_ID \
-   --region=us-central1 \
+   --region=$REGION \
    --event-notification-config=topic=projects/$PROJECT_ID/topics/telemetry
 echo 'handling auth'
 openssl req -x509 -newkey rsa:2048 -keyout rsa_private.pem -nodes -out rsa_cert.pem -subj "/CN=unused"
--- gce/run.sh
+++ gce/run.sh
@@ -1,20 +1,21 @@
 #!/bin/bash
 
-echo 'creating device ' $0
-gcloud beta iot devices create $0 \
+
+echo 'creating device' $1
+gcloud beta iot devices create $1 \
   --project=$PROJECT_ID \
   --region=$REGION \
   --registry=simulation \
   --public-key path=rsa_cert.pem,type=rs256
-echo 'sending signals ' $1
+echo 'sending signals' $2
 python mqtt.py \
    --project_id=$PROJECT_ID \
    --cloud_region=$REGION \
    --registry_id=simulation \
-   --device_id=$0 \
+   --device_id=$1 \
    --private_key_file=rsa_private.pem \
    --message_type=event \
-   --num_messages=$1
+   --num_messages=$2 \
    --algorithm=RS256
    
 
