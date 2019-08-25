38fb99e2302272d37aded5c6698e8c0609c064e6
vg <vsevolod.hrechaniuk@avalonsolutions.com>
2019-08-25 14:35:32 +0200

    bash imprive device id

--- gce/run_one.sh
+++ gce/run_one.sh
@@ -1,8 +1,8 @@
 #!/bin/bash
 
-
-echo 'creating device' $1
-gcloud beta iot devices create $1 \
+ID="device-$1-$(date +%s000)"
+echo 'creating device' $ID
+gcloud beta iot devices create $ID \
   --project=$PROJECT_ID \
   --region=$REGION \
   --registry=simulation \
@@ -12,7 +12,7 @@ python mqtt.py \
    --project_id=$PROJECT_ID \
    --cloud_region=$REGION \
    --registry_id=simulation \
-   --device_id=$1 \
+   --device_id=$ID \
    --private_key_file=rsa_private.pem \
    --message_type=event \
    --num_messages=$2 \
